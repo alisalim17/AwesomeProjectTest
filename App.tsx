2b0d74ad43b7a56c00ebad40cd11badbedb8bb98
alisalim17 <alisalimli1899@gmail.com>
2022-06-30 15:58:59 +0400

    counter example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import * as React from "react";
+import tw from "twrnc";
 
 export default function App() {
+  const [counter, setCounter] = React.useState(0);
+  console.log(counter);
+  const handlePlus = () => {
+    setCounter(counter + 1);
+    console.log("+++++++");
+  };
+  const handleMinus = () => {
+    setCounter(counter - 1);
+    console.log("---------");
+  };
+
+  const handleAlert = () => {
+    alert("Yes sir");
+    console.log("hey");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex-1 justify-center items-center`}>
+      <StatusBar style="dark" backgroundColor="#7ecf99" />
+      <Text style={tw`mt-2 text-purple-600 font-bold italic text-4xl `}>
+        {counter}
+      </Text>
+      <View style={tw`flex flex-row  justify-between`}>
+        <TouchableOpacity style={tw`mr-4`} onPress={handleAlert}>
+          <Text>Alert me</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={tw`mr-4`} onPress={handlePlus}>
+          <Text>Increment</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={handleMinus}>
+          <Text>Decrease</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
