7b30b8f575319a8b56e4600a0fecb39e58f4c142
alisalim17 <alisalimli1899@gmail.com>
2022-07-01 21:42:02 +0400

    scroll view

--- App.tsx
+++ App.tsx
@@ -7,45 +7,58 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 import * as React from "react";
 import tw from "twrnc";
 
 export default function App() {
-  const [counter, setCounter] = React.useState(0);
-  console.log(counter);
-  const handlePlus = () => {
-    setCounter(counter + 1);
-    console.log("+++++++");
-  };
-  const handleMinus = () => {
-    setCounter(counter - 1);
-    console.log("---------");
-  };
+  const [data, setData] = React.useState([
+    {
+      name: "Apple",
+      id: 1,
+    },
+    {
+      name: "Banana",
+      id: 2,
+    },
 
-  const handleAlert = () => {
-    alert("Yes sir");
-    console.log("hey");
-  };
+    {
+      name: "Orange",
+      id: 3,
+    },
 
+    {
+      name: "Kiwi",
+      id: 4,
+    },
+    {
+      name: "Plum",
+      id: 5,
+    },
+    {
+      name: "Mango",
+      id: 6,
+    },
+    {
+      name: "Watemelon",
+      id: 7,
+    },
+    {
+      name: "Melon",
+      id: 8,
+    },
+  ]);
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
+    <View style={tw`flex-1`}>
       <StatusBar style="dark" backgroundColor="#7ecf99" />
-      <Text style={tw`mt-2 text-purple-600 font-bold italic text-4xl `}>
-        {counter}
-      </Text>
-      <View style={tw`flex flex-row  justify-between`}>
-        <TouchableOpacity style={tw`mr-4`} onPress={handleAlert}>
-          <Text>Alert me</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={tw`mr-4`} onPress={handlePlus}>
-          <Text>Increment</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={handleMinus}>
-          <Text>Decrease</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView>
+        {data.map((item) => (
+          <View style={tw`my-8 bg-lime-300 p-4 `} key={item.id}>
+            <Text>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
