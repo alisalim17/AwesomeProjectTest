90bd89c9fab6875865005aa4ef418f358f63222e
alisalim17 <alisalimli1899@gmail.com>
2022-07-04 20:45:15 +0400

    pressable

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { Modal, Switch, Text, View } from "react-native";
+import { Modal, Pressable, Switch, Text, View } from "react-native";
 
 import React, { useState } from "react";
 import tw from "twrnc";
 
 export default function App() {
-  const [isEnabled, setIsEnabled] = useState(false);
+  const [isEnabled, setIsEnabled] = useState(1);
+
   return (
     <View style={tw`flex-1 items-center justify-center pt-5`}>
       <StatusBar style="dark" backgroundColor="#7ecf99" />
-      <Switch
-        thumbColor={isEnabled ? "yellow" : "white"}
-        trackColor={{ false: "red", true: "green" }}
-        onValueChange={() => setIsEnabled(!isEnabled)}
-        value={isEnabled}
-      />
+      <Pressable
+        style={tw`bg-blue-500`}
+        hitSlop={20}
+        pressRetentionOffset={{ bottom: 100, left: 90, right: 90, top: 90 }}
+        onPress={() => {
+          setIsEnabled(isEnabled + 1);
+          console.log(isEnabled);
+        }}
+      >
+        <Text>I'm pressable!</Text>
+      </Pressable>
     </View>
   );
 }
