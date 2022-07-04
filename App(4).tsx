fa670f388793ac210228185b4cf79bcb58e514fe
alisalim17 <alisalimli1899@gmail.com>
2022-07-04 19:28:27 +0400

    flex

--- App(4).tsx
+++ App(4).tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import {
   Keyboard,
-  Text,
   TextInput,
   TouchableWithoutFeedback,
   View,
@@ -13,7 +12,11 @@ import tw from "twrnc";
 export default function App() {
   return (
     <View style={tw`flex-1 flex-row`}>
-      <Text>Main screen</Text>
+      <View style={tw`bg-blue-500 flex-9`}></View>
+      <View style={tw`bg-red-500 flex-1`}></View>
+      <View style={tw`bg-green-500 flex-1`}></View>
+      <View style={tw`bg-pink-500 flex-1`}></View>
+      <View style={tw`bg-black flex-5`}></View>
     </View>
   );
 }
