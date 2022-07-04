03d0b65510d5b3719e91ee22520cc046026c2ca4
alisalim17 <alisalimli1899@gmail.com>
2022-07-04 19:39:43 +0400

    modal

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Keyboard,
-  Text,
-  TextInput,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { Modal, Text, View } from "react-native";
 
 import * as React from "react";
 import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={tw`flex-1 flex-row`}>
+    <View style={tw`flex-1 pt-5`}>
+      <StatusBar style="dark" backgroundColor="#7ecf99" />
+
       <Text>Main screen</Text>
+      <Modal visible={false}>
+        <Text>Modal screen</Text>
+      </Modal>
     </View>
   );
 }
