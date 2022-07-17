d8bde14fbef35ad196c564f092d405da06cabae2
Ali Salimli <alisalimli1899@gmail.com>
2022-07-17 14:19:27 +0400

    animation - withSpring

--- Examples/screens/Transations/Transations.tsx
+++ Examples/screens/Transations/Transations.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Text, Touchable, View } from 'react-native'
 import Card, { assets, Cards } from '../../Card'
 import tw from 'twrnc'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import Animated, { useAnimatedStyle, withSpring } from 'react-native-reanimated'
+import { useSharedValue } from 'react-native-gallery-toolkit'
+
+const alpha = Math.PI / 6
+
 const cards = [
     Cards.Card1,
     Cards.Card2,
     Cards.Card3,
+    Cards.Card4,
+    Cards.Card5,
+    Cards.Card6,
 ]
 
-const alpha = Math.PI / 6
+const Box: React.FC<{ card: number, toggled: boolean }> = ({ card, toggled }) => {
+    // const rotation = toggled ? (card - 1) * alpha : 0
+    const rotate = useSharedValue(0)
+
+    useEffect(() => {
+        rotate.value = withSpring(toggled ? (card - 1) * alpha : 0)
+    }, [toggled])
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{
+                rotate: `${rotate.value}rad`,
+            }]
+        };
+    });
+
+    return (
+        <Animated.View style={[tw`absolute`, animatedStyles]}>
+            <Card key={`card-${card}`} card={card} />
+        </Animated.View>
+    )
+}
 
 const Transations = () => {
     console.log('ex', Cards)
@@ -20,19 +49,7 @@ const Transations = () => {
         <View style={tw`flex-1`}>
 
             <View style={tw`bg-blue-500 justify-center items-center flex-9`}>
-                {cards.map((card) => {
-                    console.log('alpha', `${(card - 1) * alpha}rad`)
-                    const rotate = toggled ? (card - 1) * alpha : 0
-                    return (
-                        <View style={[tw`absolute`, {
-                            transform: [{
-                                rotate: `${rotate}rad`,
-                            }]
-                        }]}>
-                            <Card key={`card-${card}`} card={card} />
-                        </View>
-                    )
-                }
+                {cards.slice(0,5).map((card) => <Box toggled={toggled} card={card} />
                 )}
 
             </View>
